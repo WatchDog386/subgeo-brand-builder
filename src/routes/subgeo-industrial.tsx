31efa5339d2ba89560bb57da4d3a15be7f42e6b5
1235-hue <berniemichael103@gmail.com>
2026-06-10 21:12:38 +0300

    images

--- src/routes/subgeo-industrial.tsx
+++ src/routes/subgeo-industrial.tsx
@@ -53,7 +53,7 @@ import constructionSiteImg from "@/assets/industry.jpeg";
 import teamWorkingImg from "@/assets/work.jpg";
 import equipmentImg from "@/assets/equipment.jpg";
 import safetyGearImg from "@/assets/safety.jpg";
-import blueprintImg from "@/assets/blueprint.png";
+import blueprintImg from "@/assets/siteassessment.png";
 import completedProjectImg from "@/assets/completeprjct.jpg";
 
 export const Route = createFileRoute("/subgeo-industrial")({
